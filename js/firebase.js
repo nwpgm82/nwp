64e9a44d48e24ea1d5c18ab05fe97baf98e39d5d
nwpgm82 <66294174+nwpgm82@users.noreply.github.com>
2021-03-26 03:12:33 +0700

    install firebase

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCoCjFOCsEkP4iVi3_p3ZUce1t3mkP26I8",
+    authDomain: "nwps-8f32b.firebaseapp.com",
+    projectId: "nwps-8f32b",
+    storageBucket: "nwps-8f32b.appspot.com",
+    messagingSenderId: "633451765264",
+    appId: "1:633451765264:web:190b48f0d7d01a91c53495",
+    measurementId: "G-KDT2HGC6HV"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
